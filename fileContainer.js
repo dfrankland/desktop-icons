c1698d03a46ada3851fc2b940ededf4919f2c07a
Carlos Soriano <csoriano@redhat.com>
2018-08-06 20:08:01 +0200

    fileContainer: Fix function callback typo

--- fileContainer.js
+++ fileContainer.js
@@ -177,7 +177,7 @@ var FileContainer = new Lang.Class (
         }
         this._menu = new PopupMenu.PopupMenu(this.actor, 0.5, side);
         this._menu.addAction(_("Open"), () => this._openOnClicked());
-        this._menu.addAction(_("Copy"), () => this._copyOnclicked());
+        this._menu.addAction(_("Copy"), () => this._copyOnClicked());
         this._menu.addAction(_("Move to Trash"), () => this._moveToTrashOnClicked());
         this._menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         this._menu.addAction(_("Properties"), () => this._propertiesOnClicked());
