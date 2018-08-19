7eef1844cb635d28d793b57ec695dcd5573ca6c7
Carlos Soriano <csoriano@redhat.com>
2018-08-19 07:41:14 +0200

    dbusUtils: Use string templates

--- dbusUtils.js
+++ dbusUtils.js
@@ -1,28 +1,28 @@
 const Gio = imports.gi.Gio;
 
-const NautilusFileOperationsInterface = '<node>\
-<interface name="org.gnome.Nautilus.FileOperations"> \
-    <method name="CopyURIs"> \
-        <arg name="URIs" type="as" direction="in"/> \
-        <arg name="Destination" type="s" direction="in"/> \
-    </method> \
-    <method name="MoveURIs"> \
-        <arg name="URIs" type="as" direction="in"/> \
-        <arg name="Destination" type="s" direction="in"/> \
-    </method> \
-    <method name="TrashFiles"> \
-        <arg name="URIs" type="as" direction="in"/> \
-    </method> \
-    <method name="CreateFolder"> \
-        <arg name="URI" type="s" direction="in"/> \
-    </method> \
-    <method name="Undo"> \
-    </method> \
-    <method name="Redo"> \
-    </method> \
-    <property name="UndoStatus" type="i" access="read"/>\
-</interface> \
-</node>';
+const NautilusFileOperationsInterface = `<node>
+<interface name="org.gnome.Nautilus.FileOperations"> 
+    <method name="CopyURIs"> 
+        <arg name="URIs" type="as" direction="in"/> 
+        <arg name="Destination" type="s" direction="in"/> 
+    </method> 
+    <method name="MoveURIs"> 
+        <arg name="URIs" type="as" direction="in"/> 
+        <arg name="Destination" type="s" direction="in"/> 
+    </method> 
+    <method name="TrashFiles"> 
+        <arg name="URIs" type="as" direction="in"/> 
+    </method> 
+    <method name="CreateFolder"> 
+        <arg name="URI" type="s" direction="in"/> 
+    </method> 
+    <method name="Undo"> 
+    </method> 
+    <method name="Redo"> 
+    </method> 
+    <property name="UndoStatus" type="i" access="read"/>
+</interface> 
+</node>`;
 
 const NautilusFileOperationsProxyInterface = Gio.DBusProxy.makeProxyWrapper(NautilusFileOperationsInterface);
 
@@ -39,18 +39,18 @@ var NautilusFileOperationsProxy = new NautilusFileOperationsProxyInterface(
     }
 );
 
-const FreeDesktopFileManagerInterface = '<node>\
-<interface name="org.freedesktop.FileManager1"> \
-    <method name="ShowItems"> \
-        <arg name="URIs" type="as" direction="in"/> \
-        <arg name="StartupId" type="s" direction="in"/> \
-    </method> \
-    <method name="ShowItemProperties"> \
-        <arg name="URIs" type="as" direction="in"/> \
-        <arg name="StartupId" type="s" direction="in"/> \
-    </method> \
-</interface> \
-</node>';
+const FreeDesktopFileManagerInterface = `<node>
+<interface name="org.freedesktop.FileManager1"> 
+    <method name="ShowItems"> 
+        <arg name="URIs" type="as" direction="in"/> 
+        <arg name="StartupId" type="s" direction="in"/> 
+    </method> 
+    <method name="ShowItemProperties"> 
+        <arg name="URIs" type="as" direction="in"/> 
+        <arg name="StartupId" type="s" direction="in"/> 
+    </method> 
+</interface> 
+</node>`;
 
 const FreeDesktopFileManagerProxyInterface = Gio.DBusProxy.makeProxyWrapper(FreeDesktopFileManagerInterface);
 
