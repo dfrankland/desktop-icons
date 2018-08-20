694e98e4b27c3c694dc389c18cc596535daf6999
Carlos Soriano <csoriano@redhat.com>
2018-08-20 18:57:06 +0200

    Fixup event checking
    Forgot to create the actual dictionary of events

--- desktopManager.js
+++ desktopManager.js
@@ -143,6 +143,9 @@ var DesktopManager = class {
                 }
                 // Only get a subset of events we are interested in.
                 // Note that CREATED will emit a CHANGES_DONE_HINT
+                let {
+                    CHANGES_DONE_HINT, DELETED, RENAMED, MOVED_IN, MOVED_OUT
+                } = Gio.FileMonitorEvent;
                 if (![CHANGES_DONE_HINT, DELETED, RENAMED,
                     MOVED_IN, MOVED_OUT].includes(eventType))
                     return;
