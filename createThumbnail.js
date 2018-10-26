3eeae7cee71ba47e1d6fcc2a3f3a9578ab5ace37
Carlos Soriano <csoriano@redhat.com>
2018-10-26 10:30:59 +0200

    createThumbnail: rename accessTime to modifiedTime
    It's a typo

--- createThumbnail.js
+++ createThumbnail.js
@@ -27,9 +27,9 @@ let file = Gio.File.new_for_path(ARGV[0]);
 let fileUri = file.get_uri();
 
 let fileInfo = file.query_info('standard::content-type,time::modified', Gio.FileQueryInfoFlags.NONE, null);
-let accessTime = fileInfo.get_attribute_uint64('time::modified');
+let modifiedTime = fileInfo.get_attribute_uint64('time::modified');
 let thumbnailPixbuf = thumbnailFactory.generate_thumbnail(fileUri, fileInfo.get_content_type());
 if (thumbnailPixbuf == null)
-    thumbnailFactory.create_failed_thumbnail(fileUri, accessTime);
+    thumbnailFactory.create_failed_thumbnail(fileUri, modifiedTime);
 else
-    thumbnailFactory.save_thumbnail(thumbnailPixbuf, fileUri, accessTime);
+    thumbnailFactory.save_thumbnail(thumbnailPixbuf, fileUri, modifiedTime);
